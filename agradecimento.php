a464d3b6b4dcd9099fd3b20acd93ef3bbec651d2
karlacorrea <47987828+karlacorrea@users.noreply.github.com>
2021-06-24 12:29:54 -0300

    int

---
+++ agradecimento.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+       
+<meta name="description" content="" />
+<meta name="author" content="" />
+<title>Conexão Brasil-Líbano</title>
+<link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />
+        <!-- Font Awesome icons (free version)-->
+<script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet"
+            type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+<link href="css/styles.css" rel="stylesheet" />
+
+    
+    <body id="page-top">
+        <header class="fixed-top right p-2">
+            <a href="index.html" class="btn btn-warning">Voltar</a>
+    <title>Agradecimento</title>
+</head>
+<body>
+        
+<section>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 order-lg-1">
+                <div class="p-5">
+                    <img class="img-fluid" src="assets/img/header-bg1.jpg" alt="Bi">
+                </div>
+            </div>
+            <div class="col-lg-6 order-lg-2">
+                <div class="p-5">
+                    <h2 class="display-4">Agradecemos o apoio</h2>
+                    <p>Você acaba de ajudar nos trabalhos de responsabilidade social, que resultarão em doações para comunidade brasileira no
+                    Líbano. Serão doados cestas básicas, leite, fraldas e absorventes.
+                    
+                    Fique atento no sorteio. Será no dia 04 de outubro de 2020, às 20h, no Facebook da Lady's Mall.
+                    
+                    Agradecemos o seu pagamento. Sua transação foi concluída e um recibo da compra foi enviado por e-mail para você.
+                    
+                    Agora é só torcer.
+.
+                        <br>
+                    </p>
+
+
+
+
+
+
+
+</body>
+</html>
+
